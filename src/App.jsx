5571cc9fe9dd9341a7ce28a47464a31229a2d75f
lawanaji <lawanmustaphaaji2015@gmail.com>
2024-03-31 17:03:14 +0100

    first

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Note from './Componant/Note'
+
+function App() {
+
+
+  return (
+    <>
+           <Note />
+    </>
+  )
+}
+
+export default App
---
+++ src/Componant/Note.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Note = () => {
+  return (
+    <div>
+      this is note pad
+    </div>
+  )
+}
+
+export default Note
